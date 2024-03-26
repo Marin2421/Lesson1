2192c3f6a834bd78cc1f8f19cdd049dcb1772c17
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-03-26 13:38:48 +0300

    Добавьте файлы проекта.

---
+++ Lesson1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите величину в милиметрах:");
+double mm = int.Parse(Console.ReadLine());
+double cm = mm / 10;
+double m = mm / 1000;
+double km = mm / 1000000;
+double mili = mm / 1609344;
+double ird = mm / 914.4;
+Console.WriteLine($"Сантиметры: {cm}. Метры: {m}. Километры: {km}. Мили: {mili}. Ярды: {ird}.");
